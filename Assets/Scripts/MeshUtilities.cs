d5056454bf6bb528066e0bddadb928e0873ea809
andras <andras.szabo.fin@gmail.com>
2019-05-23 20:46:21 +0200

    Calculate cut normal

--- Assets/Scripts/MeshUtilities.cs
+++ Assets/Scripts/MeshUtilities.cs
@@ -5,13 +5,25 @@ public static class MeshUtilities
 {
 	public static void LogIf(bool log, string msg)
 	{
-		if (log) { Debug.Log(msg); }
+		if (log) { Debug.LogWarning(msg); }
 	}
 
 	public static void SliceSingleTriangleMesh(GameObject meshGO, Vector3 cutStartWorldPos, Vector3 cutEndWorldPos, bool log = true)
 	{
-		TransformCutToObjectSpace(ref cutStartWorldPos, ref cutEndWorldPos, meshGO.transform);
+		var meshTransform = meshGO.transform;
+		TransformCutToObjectSpace(ref cutStartWorldPos, ref cutEndWorldPos, meshTransform);
 		LogIf(log, string.Format("Cut: {0} -> {1}", cutStartWorldPos, cutEndWorldPos));
+
+		var cutNormal = CalculateCutNormal(cutStartWorldPos, cutEndWorldPos, meshTransform.forward, log);
+		LogIf(log, string.Format("Cut normal: {0}", cutNormal));
+	}
+
+	private static Vector3 CalculateCutNormal(Vector3 cutStartObjectSpace, Vector3 cutEndObjectSpace, Vector3 localForward, bool log)
+	{
+		var delta = cutEndObjectSpace - cutStartObjectSpace;
+		LogIf(log, string.Format("Delta: {0}", delta));
+		var rotation = Quaternion.AngleAxis(90f, localForward);
+		return rotation * delta;
 	}
 
 	private static void TransformCutToObjectSpace(ref Vector3 cutStartWorldPos, ref Vector3 cutEndWorldPos, Transform meshTransform)
